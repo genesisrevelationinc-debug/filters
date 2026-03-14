7991214e6304daad9e23bc96eb9bdcd878d8d45a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-14 18:48:33 -0400

    fix: apply solution for issue #327

--- src/filters/drop-shadow/DropShadowFilter.ts
+++ src/filters/drop-shadow/DropShadowFilter.ts
@@ -25,41 +25,11 @@ import { settings } from '@pixi/settings';
     apply(filterManager, input, output, clear): void
     {
         const resolution = filterManager.renderer.resolution;
+        const cachePadding = this.padding * resolution;
+        const quality = this.quality;
 
-        const cos = Math.cos(this.angle) * this.distance * resolution;
-        const sin = Math.sin(this.angle) * this.distance * resolution;
-
-        for (let i = 0; i < this.quality; i += 1)
-        {
-            this.uniforms.blur = (i + 1) / (this.quality + 1) * this.blur * resolution;
-            this.uniforms.offset.x = cos / (i + 1);
-            filterManager.applyFilter(this, input, output, clear);
+        for (let i = 0; i < quality; i++)
+        {   
+            filterManager.applyFilter(this, input, tempRenderTexture, clear);
+            filterManager.applyFilter(this.blur, tempRenderTexture, input);
             clear = true;
-        }
-
-        if (this.shadowOnly) {
-            filterManager.applyFilter(this, output, input, clear);
-        }
-    }
-
-    /**
-        this.updatePadding();
-    }
-
-    set quality(value: number) {
-        this._quality = value;
-    }
-
-    /**
-     * The quality of the shadow. A higher value will give a smoother shadow (but at the cost of performance).
-     *
-     */
-    get quality(): number
-    {
-        return this._quality;
-    }
-
-    private set _quality(value: number) {
-        this._quality = value;
-        this.updatePadding();
-    }
