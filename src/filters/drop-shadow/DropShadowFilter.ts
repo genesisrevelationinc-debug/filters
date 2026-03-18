192e82afb282b031a13a79045aa17f5f79f18066
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 01:07:11 -0400

    fix: apply solution for issue #327

--- src/filters/drop-shadow/DropShadowFilter.ts
+++ src/filters/drop-shadow/DropShadowFilter.ts
@@ -1,17 +1,28 @@
-export class DropShadowFilter extends Filter
-{
-    private static readonly DEFAULT_FRAGMENT = `
-#ifdef GL_FRAGMENT_PRECISION_HIGH
-precision highp float;
-#else
-precision mediump float;
+import { Filter, Sprite } from '@pixi/core';
+import { Point } from '@pixi/math';
+import { settings } from '@pixi/settings';
+
+/**
+ * DropShadowFilter.
+    public distance: number;
+    public angle: number;
+    public shadowOnly: boolean;
+    public quality: number;
+
+    constructor(distance = 5, angle = Math.PI / 4, color = 0x000000, alpha = 0.5, blur = 2, quality = 1, resolution = settings.RESOLUTION, shadowOnly = false)
     {
-        super(null, DropShadowFilter.DEFAULT_FRAGMENT, { mapSampler: 0 });
-
-        // Optimize for lower performance devices
-        this.resolution = 0.5;
-        this.padding = 10;
-
-        this.uniforms.distance = 5;
-        this.uniforms.angle = Math.PI / 4;
-        this.uniforms.color = [0.0, 0.0, 0.0, 0.5];
+        super();
+        this.angle = angle;
+        this.color = color;
+        this.alpha = alpha;
+        this.quality = quality;
+        this.blur = blur;
+        this.resolution = resolution;
+        this.shadowOnly = shadowOnly;
+        this.uniforms.distance = this.distance * this.resolution;
+        this.uniforms.angle = this.angle;
+        this.uniforms.color = this.color;
+        this.uniforms.quality = this.quality;
+        this.uniforms.alpha = this.alpha;
+        this.uniforms.shadowOnly = this.shadowOnly;
+    }
